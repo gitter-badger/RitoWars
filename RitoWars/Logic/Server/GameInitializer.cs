ea4d67fbd4a4f4156b90c1e41e16c65df2f35648
eddy5641 <eddy5641@gmail.com>
2015-08-15 17:21:10 -0700

    Ok add udpclient

--- RitoWars/Logic/Server/GameInitializer.cs
+++ RitoWars/Logic/Server/GameInitializer.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using RitoWars.Logic.Game.Players;
 
 namespace RitoWars.Logic.Server
 {
     public class GameInitializer
     {
-        public GameInitializer(IPEndPoint ipEndPoint, string serverKey, )
+        public GameInitializer(IPEndPoint ipEndPoint, string serverKey, List<Player> teamBluePlayers, List<Player> teamRedPlayers)
         {
-            
+            var server = new UdpClient(ipEndPoint);
+
         }
     }
 }
